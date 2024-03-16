0341ffb1f686688a5dc01741468ff434e0c6ee3f
lanang <lananglanusaputera@gmail.com>
2024-03-17 01:05:30 +0800

    lanang

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -34,7 +34,13 @@ export default function Page() {
               {/* judul blog */}
               <h1 className="mb-4 font-semibold text-5xl">Bali merupakan tempat berwisata terbaik yang sudah terkenal diseluruh dunia</h1>
               {/* pembuat blog + tanggal */}
-              <p className="mb-8">Oleh <span className="text-primary">infoCylare.id</span> - Jumat, 15 Maret 2024</p>
+              <div className="flex flex-wrap justify-between items-center mb-8">
+                <p>Oleh <span className="text-primary">infoCylare.id</span> - Jumat, 15 Maret 2024</p>
+                <div className="flex items-center gap-4">
+                  <p><i className="fa fa-fw fa-sm fa-heart text-red-500"/> 1.285</p>
+                  <p><i className="fa fa-fw fa-sm fa-comment"/> 538</p>
+                </div>
+              </div>
 
               {/* gambar */}
               <div>
@@ -81,6 +87,114 @@ export default function Page() {
 
               <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum atque quaerat libero quo aliquid natus aliquam est fugit dolorem, cum blanditiis sapiente explicabo accusamus ut sint expedita doloremque sequi laborum deleniti, quidem reiciendis sed, veniam iure provident. Repellat debitis rem asperiores, commodi quibusdam eveniet illum culpa? Hic a ex explicabo vero! Eum laborum molestias dolorem, accusamus ipsum nobis at perferendis.</p>
               <p className="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, atque id pariatur quas quasi voluptas quod? Tempora ad non nulla deleniti fugit officia iure architecto minus sed sequi unde esse et, quidem maiores optio totam hic nemo illum nobis beatae! Sit architecto vero facilis odit quidem, fugiat ullam nemo nostrum eius aliquid inventore, ut rerum! Voluptatem exercitationem recusandae numquam ab veritatis vitae magni deleniti nihil fuga odio, enim, asperiores debitis.</p>
+
+              <div className="mt-8">
+                <p className="text-2xl font-semibold">Suka dengan postingan ini?</p>
+                <p className="mb-4">klik tombol dibawah untuk menyukai postingan ini.</p>
+                <button type="button" className="px-4 py-1 text-sm bg-gradient-to-b from-red-500 to-red-600 rounded-lg shadow-lg shadow-black/5 text-white"><i className="fa fa-fw fa-sm fa-heart text-white"/> Sukai</button>
+                {/* <button type="button" className="px-4 py-1 text-sm bg-gradient-to-b from-red-500 to-red-600 rounded-lg shadow-lg shadow-black/5 text-white"><i className="fa fa-fw fa-sm fa-check text-white"/> Disukai</button> */}
+              </div>
+
+              <div className="border-t border-zinc-400 pt-8 mt-8">
+                <div className="flex flex-wrap items-center gap-2 mb-4">
+                  <p className="text-xs px-4 py-1 rounded-lg shadow-lg shadow-black/5 bg-gradient-to-b from-zinc-600 to-zinc-700 text-white"><i className="fa fa-fw fa-sm fa-tag"/> Tags</p>
+                  <Link href="#" className="text-xs px-4 py-1 rounded-lg shadow-lg shadow-black/5 bg-gradient-to-b from-blue-500 to-blue-600 text-white">infoDenpasar</Link>
+                  <Link href="#" className="text-xs px-4 py-1 rounded-lg shadow-lg shadow-black/5 bg-gradient-to-b from-blue-500 to-blue-600 text-white">denpasar</Link>
+                  <Link href="#" className="text-xs px-4 py-1 rounded-lg shadow-lg shadow-black/5 bg-gradient-to-b from-blue-500 to-blue-600 text-white">liburan</Link>
+                  <Link href="#" className="text-xs px-4 py-1 rounded-lg shadow-lg shadow-black/5 bg-gradient-to-b from-blue-500 to-blue-600 text-white">wisata</Link>
+                </div>
+
+                <div className="grid grid-cols-2 gap-3 my-8">
+                  <Link href="#" className="group/new block border-s border-zinc-400 p-4">
+                    <p className="mb-2">Postingan sebelumnya</p>
+                    <p className="font-medium group-hover/new:text-blue-500 line-clamp-3 duration-200">Cara menggunakan ChatGPT terbaru yang akan rilis pada bulan maret ini</p>
+                  </Link>
+                  <Link href="#" className="group/new block border-s border-zinc-400 p-4">
+                    <p className="mb-2">Postingan selanjutnya</p>
+                    <p className="font-medium group-hover/new:text-blue-500 line-clamp-3 duration-200">berikut merupakan cara terbaru untuk cepat mendapatkan savage di mobile legends</p>
+                  </Link>
+                </div>
+
+                <p className="mb-3">Berita terkini</p>
+                <div className="grid grid-cols-3 gap-3">
+                  <div>
+                    <Link href="#" className="group/new relative block">
+                      <div className="absolute z-20 start-0 top-0 rounded-tl rounded-br-lg bg-gradient-to-b from-yellow-500 to-yellow-600 text-white text-xs shadow-lg shadow-black/25 px-4 py-0.5">
+                        <p>Terkini</p>
+                      </div>
+                      <div className="relative overflow-hidden rounded-lg">
+                        <img className="group-hover/new:scale-105 duration-200 aspect-[3/2] object-cover object-center rounded-lg" src="https://c4.wallpaperflare.com/wallpaper/179/915/685/photography-water-reflection-bali-wallpaper-preview.jpg" alt="Image1" />
+                        <div className="group-hover/new:opacity-100 opacity-0 duration-200 bg-black/25 text-white z-10 absolute inset-0 flex justify-center items-center">
+                          <div className="text-center">
+                            <i className="fa fa-fw fa-eye"/>
+                            <p className="text-sm">Klik untuk selengkapnya...</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="pt-4">
+                        <h3 className="mb-2 line-clamp-5 font-medium">Bali merupakan tempat berwisata terbaik yang sudah terkenal diseluruh dunia</h3>
+                        <p className="lg:text-[0.8vw]"><span className="text-primary">infoCylare.id</span> - Jumat, 15 Maret 2024</p>
+                      </div>
+                    </Link>
+                  </div>
+                  <div>
+                    <Link href="#" className="group/new relative block">
+                      <div className="absolute z-20 start-0 top-0 rounded-tl rounded-br-lg bg-gradient-to-b from-yellow-500 to-yellow-600 text-white text-xs shadow-lg shadow-black/25 px-4 py-0.5">
+                        <p>Terkini</p>
+                      </div>
+                      <div className="relative overflow-hidden rounded-lg">
+                        <img className="group-hover/new:scale-105 duration-200 aspect-[3/2] object-cover object-center rounded-lg" src="https://c4.wallpaperflare.com/wallpaper/179/915/685/photography-water-reflection-bali-wallpaper-preview.jpg" alt="Image1" />
+                        <div className="group-hover/new:opacity-100 opacity-0 duration-200 bg-black/25 text-white z-10 absolute inset-0 flex justify-center items-center">
+                          <div className="text-center">
+                            <i className="fa fa-fw fa-eye"/>
+                            <p className="text-sm">Klik untuk selengkapnya...</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="pt-4">
+                        <h3 className="mb-2 line-clamp-5 font-medium">Bali merupakan tempat berwisata terbaik yang sudah terkenal diseluruh dunia</h3>
+                        <p className="lg:text-[0.8vw]"><span className="text-primary">infoCylare.id</span> - Jumat, 15 Maret 2024</p>
+                      </div>
+                    </Link>
+                  </div>
+                  <div>
+                    <Link href="#" className="group/new relative block">
+                      <div className="absolute z-20 start-0 top-0 rounded-tl rounded-br-lg bg-gradient-to-b from-yellow-500 to-yellow-600 text-white text-xs shadow-lg shadow-black/25 px-4 py-0.5">
+                        <p>Terkini</p>
+                      </div>
+                      <div className="relative overflow-hidden rounded-lg">
+                        <img className="group-hover/new:scale-105 duration-200 aspect-[3/2] object-cover object-center rounded-lg" src="https://c4.wallpaperflare.com/wallpaper/179/915/685/photography-water-reflection-bali-wallpaper-preview.jpg" alt="Image1" />
+                        <div className="group-hover/new:opacity-100 opacity-0 duration-200 bg-black/25 text-white z-10 absolute inset-0 flex justify-center items-center">
+                          <div className="text-center">
+                            <i className="fa fa-fw fa-eye"/>
+                            <p className="text-sm">Klik untuk selengkapnya...</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="pt-4">
+                        <h3 className="mb-2 line-clamp-5 font-medium">Bali merupakan tempat berwisata terbaik yang sudah terkenal diseluruh dunia</h3>
+                        <p className="lg:text-[0.8vw]"><span className="text-primary">infoCylare.id</span> - Jumat, 15 Maret 2024</p>
+                      </div>
+                    </Link>
+                  </div>
+                </div>
+                <div className="mt-8">
+                  <form className="grid grid-cols-2 gap-3">
+                    <div className="relative">
+                      <input type="text" id="namaLengkap" className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-zinc-200 rounded-lg border border-zinc-400 appearance-none focus:outline-none focus:ring-0 focus:border-blue-500 peer" placeholder=" " />
+                      <label htmlFor="namaLengkap" className="absolute text-sm duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-zinc-200 px-2 peer-focus:px-2 peer-focus:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Nama lengkap</label>
+                    </div>
+                    <div className="relative">
+                      <input type="text" id="email" className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-zinc-200 rounded-lg border border-zinc-400 appearance-none focus:outline-none focus:ring-0 focus:border-blue-500 peer" placeholder=" " />
+                      <label htmlFor="email" className="absolute text-sm duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-zinc-200 px-2 peer-focus:px-2 peer-focus:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Email</label>
+                    </div>
+                    <div className="col-span-2 relative">
+                      <textarea id="komentar" className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-zinc-200 rounded-lg border border-zinc-400 appearance-none focus:outline-none focus:ring-0 focus:border-blue-500 peer" placeholder=" " />
+                      <label htmlFor="komentar" className="absolute text-sm duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-zinc-200 px-2 peer-focus:px-2 peer-focus:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Ketik komentar anda disini...</label>
+                    </div>
+                  </form>
+                </div>
+              </div>
 
             </div>
           </div>
